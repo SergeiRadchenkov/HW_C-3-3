683ce755c0079d4c1f73fbc7caa20a2e1dce947a
Sergei <radchenkovsa@yandex.ru>
2023-08-29 19:08:13 +0300

    Case23

---
+++ case23/Program.cs
@@ -0,0 +1,23 @@
+﻿/* Напишите программу, которая принимает на 
+вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+3 -> 1, 4, 9.
+5 -> 1, 8, 27, 64, 125. */
+
+int InputNum(string message)
+{
+    Console.WriteLine(message);
+    return int.Parse(Console.ReadLine()!);
+}
+
+void Cubes(int num)
+{
+    for (int i = 1; i <= num; i++)
+    {
+        int res = (int)Math.Pow(i, 3);
+        Console.Write(res);
+        if (i != num) Console.Write(", ");
+    }
+}
+
+int number = InputNum("Введите число: ");
+Cubes(number);
